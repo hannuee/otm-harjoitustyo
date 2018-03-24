f4f9c48f4f17c9a8ac2012abb9d5fd5bddb37daa
Hannu Erälaukko <32358926+hannuee@users.noreply.github.com>
2018-03-24 16:11:34 +0200

    Edistetty pelilogiikkaa

--- FortressDuel/src/main/java/otmharjoitustyo/logic/Game.java
+++ FortressDuel/src/main/java/otmharjoitustyo/logic/Game.java
@@ -3,13 +3,28 @@
  */
 package otmharjoitustyo.logic;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 public class Game {
     
-    int[][] situation;  // private? vai public jotta ei tuu turhia kopiointeja?
-    boolean simulationInProgress; // private!
-    boolean leftTurn;
+    BufferedImage gameField;
+    private final int groundLevel;
+    
+    /*
+    States:
+    0 = Game over.
+    1 = Left player's turn.
+    2 = Simulating the effects of an ammunition shot by the left player.
+    3 = Right player's turn.
+    4 = Simulating the effects of an ammunition shot by the right player.
+    */
+    int state;
+    
+    private static final int AMMUNITION_RADIUS = 7;
+    private static final int EXPLOSION_RADIUS = 7;
    
-    int leftCannonX;  // private final?
+    int leftCannonX;
     int leftCannonY;
     int rightCannonX;
     int rightCannonY;
@@ -17,17 +32,56 @@ public class Game {
     double initialVx;
     double initialVy;
     
+    boolean oldAmmunitionExist;
+    int oldAmmunitionX;
+    int oldAmmunitionY;
+    
     // Tarkastetaan törmäykset maahan rajoista?
     
     // Törmäykset linnaan erikseen?
     
+    /**
+     * 
+     * @param gameField Image of the Game Field.
+     * @param groundLevel The height of ground level in pixels from the bottom of the image.
+     */
+    public Game(BufferedImage gameField, int groundLevel, int leftCannonX, int leftCannonY, int rightCannonX, int rightCannonY){
+        this.gameField = gameField;
+        this.groundLevel = groundLevel;
+        
+        this.leftCannonX = leftCannonX;
+        this.leftCannonY = leftCannonY;
+        this.rightCannonX = rightCannonX;
+        this.rightCannonY = rightCannonY;
+        
+        this.state = 1;
+        
+        this.oldAmmunitionExist = false;
+    }
+    
+    private int yFromNormalToImage(int y){
+        return this.gameField.getHeight() - y;
+    }
+    
+    private int ammunitionX(double seconds){
+        double positionDueInitialVx = this.initialVx * seconds;
+        
+        int positionDueCannonPosition;
+        if(state == 1){
+            positionDueCannonPosition = this.leftCannonX;
+        } else {
+            positionDueCannonPosition = this.rightCannonX;
+        }
+        
+        return (int)positionDueInitialVx + positionDueCannonPosition;
+    }
     
     private int ammunitionY(double seconds){
         double positionDueGravity = (-9.81/2.0) * Math.pow(seconds, 2);
         double positionDueInitialVy = this.initialVy * seconds;
         
         int positionDueCannonPosition;
-        if(leftTurn){
+        if(state == 1){
             positionDueCannonPosition = this.leftCannonY;
         } else {
             positionDueCannonPosition = this.rightCannonY;
@@ -36,17 +90,44 @@ public class Game {
         return (int)(positionDueGravity + positionDueInitialVy) + positionDueCannonPosition;
     }
     
-    private int ammunitionX(double seconds){
-        double positionDueInitialVx = this.initialVx * seconds;
+    private void removeOldAmmunition(){
+        // x2 + y2 <= 7
         
-        int positionDueCannonPosition;
-        if(leftTurn){
-            positionDueCannonPosition = this.leftCannonX;
-        } else {
-            positionDueCannonPosition = this.rightCannonX;
+    }
+    
+    public int getSimulationSnapshot(double seconds){
+        
+        // Jos ammuksella vanha sijainti niin poistetaan vanhat pixelit.
+        if(oldAmmunitionExist){
+            removeOldAmmunition();
+            oldAmmunitionExist = false;
         }
         
-        return (int)positionDueInitialVx + positionDueCannonPosition;
+        
+        // Lasketaan uusi ammuksen sijainti.
+        
+        // Tarkastetaan rajat:
+        // jos vas tai oik yli niin null
+        
+        // tarkasta osumat maahan ja linnaan kun uusia pixeleitä merkataan.
+        //      Jos pixeli osuu linnaan tai maahan niin poista linnapixelit explode-alueelta (ja explode keskus muistiin.)
+        
+        // Palautetaan tilanne.
+        
+        
+        return 0;
     }
+    
+    public int[] explosionCoordinates(){
+        return null;
+    }
+    
+    public boolean isGameOver(){
+        if(this.state == 0){
+            return true;
+        }
+        return false;
+    }
+    
     
 }
